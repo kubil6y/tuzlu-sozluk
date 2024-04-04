2f11aeb1fd66676fc8a2f510c7b892bd1b38d51a
kubil6y <lieqb2@gmail.com>
2024-04-04 12:37:23 +0300

    add search input

--- app/(platform)/_components/search-bar.tsx
+++ app/(platform)/_components/search-bar.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { useEffect, useState, useRef } from "react";
 import { useDebounce, useClickAway } from "react-use/lib";
+import { SearchIcon } from "lucide-react";
+import { Icons } from "@/components/icons";
 
 type PostResult = {
     id: string;
@@ -19,12 +22,12 @@ export const SearchBar = () => {
     const [debouncedValue, setDebouncedValue] = useState("");
     const clickAwayRef = useRef(null);
 
-    useClickAway(clickAwayRef, (e) => {
+    useClickAway(clickAwayRef, () => {
         setShowPosts(false);
     });
 
     const debounceInterval = 400;
-    const [, cancel] = useDebounce(
+    useDebounce(
         () => {
             setDebouncedValue(inputText);
         },
@@ -40,6 +43,7 @@ export const SearchBar = () => {
             }
 
             try {
+                setIsLoading(true);
                 const res = await fetch(
                     `/api/posts?searchTerm=${debouncedValue}`,
                     { cache: "no-store" }
@@ -49,52 +53,66 @@ export const SearchBar = () => {
                 setShowPosts(true);
             } catch (err) {
                 setPosts([]);
+            } finally {
+                setIsLoading(false);
             }
         };
         search();
+        if (debouncedValue.length === 0) {
+            setShowPosts(false);
+        }
     }, [debouncedValue]);
 
     return (
         <div className="w-[30%] relative" ref={clickAwayRef}>
-            <Input
-                type="text"
-                value={inputText}
-                placeholder="Search"
-                onChange={({ currentTarget }) => {
-                    setInputText(currentTarget.value);
-                }}
-            />
+            <div className="relative h-10 w-full">
+                <Input
+                    onClick={() => {
+                        if (!showPosts && debouncedValue.length > 0) {
+                            setShowPosts(true);
+                        }
+                    }}
+                    type="text"
+                    value={inputText}
+                    placeholder="Search"
+                    onChange={({ currentTarget }) => {
+                        setInputText(currentTarget.value);
+                    }}
+                />
+                <div className="absolute right-2 top-1/2 transform -translate-y-1/2 text-primary">
+                    {isLoading ? (
+                        <Icons.spin className="animate-spin size-5" />
+                    ) : (
+                        <SearchIcon className="size-5" />
+                    )}
+                </div>
+            </div>
             {showPosts && (
-                <div className="absolute bg-gray-500">
-                    {posts.length > 0 ? (
+                <div className="absolute mt-1.5 w-full border shadow-md bg-background text-foreground">
+                    {posts.length > 0 && !isLoading ? (
                         posts.map((post) => (
-                            <div key={post.id}>{post.title}</div>
+                            <Link
+                                href={`/posts/${post.slug}`}
+                                key={post.id}
+                                className="block py-1.5 px-4 cursor-pointer hover:bg-primary/80 transition hover:text-white"
+                                onClick={() => {
+                                    if (showPosts) {
+                                        setDebouncedValue("");
+                                        setInputText("");
+                                        setShowPosts(false);
+                                    }
+                                }}
+                            >
+                                {post.title}
+                            </Link>
                         ))
                     ) : (
-                        <div>no posts found</div>
+                        <div className="min-h-24 flex items-center justify-center">
+                            No results found
+                        </div>
                     )}
                 </div>
             )}
         </div>
     );
 };
-
-/*
- <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          {/* Use map to loop over the dynamic list */}
-          {fruits.map((fruit) => (
-            // Make sure to set a unique key for each SelectItem
-            <SelectItem key={fruit.id} value={fruit.id}>
-              {fruit.label}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-*/
--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,10 @@
 import { db } from "@/lib/db";
+import { TODO_sleep } from "@/lib/sleep";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+    await TODO_sleep(1000);
+
     const { searchParams } = new URL(req.url);
     const searchTerm = searchParams.get("searchTerm");
     if (!searchTerm) {
